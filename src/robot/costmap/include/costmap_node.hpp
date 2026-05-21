e1d2cbe0c811f05e957c1803899be7e93a2c8dc0
Ariel Liu <cutiepanda2468@gmail.com>
2026-05-21 16:34:06 -0400

    warmup

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,16 +1,23 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
-
+ 
 #include "rclcpp/rclcpp.hpp"
-
+#include "std_msgs/msg/string.hpp"
+ 
 #include "costmap_core.hpp"
-
+ 
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
-
+    
+    // callback function
+    void publishMessage();
+ 
   private:
     robot::CostmapCore costmap_;
+    // constructs
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
-
+ 
 #endif 
